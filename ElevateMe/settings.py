f599b4826e2a75989144f2829de29b2cb0c9e3e7
ErwinLambujon <erwinlambujon1@gmail.com>
2024-12-02 23:51:54 +0800

    Added the latest update

--- ElevateMe/settings.py
+++ ElevateMe/settings.py
@@ -140,6 +140,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIR = [
     os.path.join(BASE_DIR, 'static')
 ]
